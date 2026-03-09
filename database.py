119b68774913a1d2a177073df790efb9be42c29a
aldanaboero <106601499+aldanaboero@users.noreply.github.com>
2026-03-09 10:48:42 -0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+conn = sqlite3.connect("finance.db", check_same_thread=False)
+cursor = conn.cursor()
+
+def create_tables():
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT,
+        password TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS accounts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        name TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS incomes(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        account_id INTEGER,
+        description TEXT,
+        amount REAL,
+        method TEXT,
+        date TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS expenses(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        account_id INTEGER,
+        category TEXT,
+        description TEXT,
+        amount REAL,
+        method TEXT,
+        installments INTEGER,
+        date TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS suppliers(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        account_id INTEGER,
+        name TEXT,
+        product TEXT,
+        amount REAL,
+        status TEXT
+    )
+    """)
+
+    conn.commit()
