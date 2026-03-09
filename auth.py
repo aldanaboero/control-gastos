361544a968c72b2920e16d7e70b59659b7fe81b4
aldanaboero <106601499+aldanaboero@users.noreply.github.com>
2026-03-09 10:49:20 -0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,28 @@
+import bcrypt
+from database import cursor, conn
+
+def hash_password(password):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def verify_password(password, hashed):
+    return bcrypt.checkpw(password.encode(), hashed)
+
+def register_user(email, password):
+
+    hashed = hash_password(password)
+
+    cursor.execute(
+        "INSERT INTO users(email,password) VALUES (?,?)",
+        (email, hashed)
+    )
+
+    conn.commit()
+
+def login_user(email):
+
+    cursor.execute(
+        "SELECT * FROM users WHERE email=?",
+        (email,)
+    )
+
+    return cursor.fetchone()
